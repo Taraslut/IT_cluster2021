8b5622d62a40ba612bc1d46383f02ebe8287b66f
Taras Lutsyshyn <taraslut78@gmail.com>
2021-10-22 19:56:07 +0300

    Create add function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def print_my(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}!')  # Press ⌘F8 to toggle the breakpoint.
+
+
+def add(one, other, *args):
+    """Function for adding two aor more values."""
+    rez = one + other
+    for i in args:
+        rez += i
+    return rez
 
 
 if __name__ == '__main__':
